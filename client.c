6560ecb4f4bcdc4113b5c3f1ccc1042bc89bb3f6
Roman Bogoudinov <73884598+dwarfsuomalainen@users.noreply.github.com>
2024-07-15 14:48:32 +0300

    Update client.c

--- client.c
+++ client.c
@@ -12,8 +12,68 @@
 
 #include "minitalk.h"
 
+void err_msg_exit(int error_code)
+{
+	if (error_code == 1)
+		ft_printf("[+] Error: \033[31mNo server_id\n\033[0m \n");
+	if (error_code == 2)
+		ft_printf("[+] Error: \033[31mNo string provided\n\033[0m \n");
+	if (error_code == 3)
+		ft_printf("[+] Error: \033[31mWrong P I D\n\033[0m \n");
+	if (error_code == 4)
+		ft_printf("[+] Error: \033[31mNo message recieved\n\033[0m \n");
+	if (error_code == 5)
+		ft_printf("[+] Error: \033[31mPlease use a format :\n\
+		./client<P I D Server> <\"STRING\">\n\033[0m \n");
+	if (error_code == 6)
+		ft_printf("[+] Error: \033[31mKill signal failed\n\033[0m \n");
+	exit (0);
+}
+
+void broadcast(int pid,char *string)
+{
+	int	string_position;
+	int	bit_position;
+
+	string_position = -1;
+	while (++bit_position < 8)
+	{
+		if (((unsigned char)(string[string_position] >> (7 -bit_position)) & 1)== 0)
+		{
+			kill(pid, SIGUSR1);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+		}
+		usleep(200);
+	}
+	bit_position = 0;
+	while (bit_position++ < 8)
+	{
+		kill(pid, SIGUSR1);
+		usleep(200);
+	}
+}
+
 int main(int argc, char **argv)
 {
-    char    *string;
-    int
+    char	*string;
+    int		server_id;
+
+	if (argc == 3)
+	{
+		server_id = ft_atoi(argv[1]);
+		if (!server_id)
+			err_msg_exit(1);
+		if (kill(server_id, 0) == -1)
+			err_msg_exit(3);
+		string = argv[2];
+		if (string[0]== 0)
+			err_msg_exit(2);
+		broadcast(server_id, string);
+	}
+	else
+		err_msg_exit(5);
+	return (EXIT_SUCCESS);
 }
